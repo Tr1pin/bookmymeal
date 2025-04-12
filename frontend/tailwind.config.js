aa711cf2792556a32f8666e8727bf6941321cef6
denis <camblorcaberodenis@gmail.com>
2025-04-12 16:18:27 +0200

    Add Montserrat font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
       "./src/**/*.{html,ts}" // Asegúrate de incluir los archivos de Angular
     ],
     theme: {
+      fontFamily: {
+        'cinzel': ['Cinzel', 'sans-serif']
+      } ,
       extend: {
         colors: {
           primary: '#1E40AF', // Azul fuerte
