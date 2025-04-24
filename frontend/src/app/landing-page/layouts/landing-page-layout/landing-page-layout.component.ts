ea59859eed9f54953f489a6407c79f4cc4608857
Camblor Cabero <camblorcaberodenis@gmail.com>
2025-04-24 15:32:28 +0200

    Update and finish nav-bar styles

--- frontend/src/app/landing-page/layouts/landing-page-layout/landing-page-layout.component.ts
+++ frontend/src/app/landing-page/layouts/landing-page-layout/landing-page-layout.component.ts
@@ -1,10 +1,11 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FrontNavbarComponent } from "../../components/front-navbar/front-navbar.component";
+import { FooterLanding } from '../../components/footer-landing/front-landing.component';
 
 @Component({
   selector: 'app-landing-page-layout',
-  imports: [RouterOutlet, FrontNavbarComponent],
+  imports: [RouterOutlet, FrontNavbarComponent, FooterLanding],
   templateUrl: './landing-page-layout.component.html'
 })
 export class LandingPageLayoutComponent { }
