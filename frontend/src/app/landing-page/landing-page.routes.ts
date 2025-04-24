c1b7dcd1ddd800fcc3029b56f5c45c1d29c79df1
Camblor Cabero <camblorcaberodenis@gmail.com>
2025-04-24 17:08:15 +0200

    Add links to new components in nav-bar

--- frontend/src/app/landing-page/landing-page.routes.ts
+++ frontend/src/app/landing-page/landing-page.routes.ts
@@ -1,11 +1,42 @@
 import { Routes } from "@angular/router";
 import { LandingPageLayoutComponent } from "./layouts/landing-page-layout/landing-page-layout.component";
+import { LoginComponent } from "../auth/login/login.component";
+import { RegisterComponent } from "../auth/register/register.component";
+import { HomeComponent } from "./layouts/home/home.component";
+import { CartaComponent } from "./layouts/carta/carta.component";
+import { ReservasComponent } from "./layouts/reservas/reservas.component";
 
 
 export const landingPageRoutes: Routes = [
    {
     path: '',
     component: LandingPageLayoutComponent,
+    children: [
+         {
+            path: '',
+            component: HomeComponent
+         },
+         {
+            path: 'login',
+            component: LoginComponent
+         },
+         {
+            path: 'register',
+            component: RegisterComponent
+         },
+         {
+            path: 'carta',
+            component: CartaComponent
+         },
+         {
+            path: 'reservas',
+            component: ReservasComponent
+         },
+    ]
+   },
+   {
+      path: '**',
+      redirectTo: ''
    }
 ] 
 
