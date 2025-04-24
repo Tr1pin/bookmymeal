8c1282ecb2bed37fe42a196ec4e8e11a8325ea7c
Camblor Cabero <camblorcaberodenis@gmail.com>
2025-04-24 17:04:57 +0200

    Add basic carta component

---
+++ frontend/src/app/landing-page/layouts/carta/carta.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carta',
+  imports: [],
+  templateUrl: './carta.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CartaComponent { }
