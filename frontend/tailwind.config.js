9086ea9e01262c0825757ac295b3c2e947bd7021
Camblor Cabero <denis.camblor-cabero.external@capgemini.com>
2025-04-24 15:33:24 +0200

    update config files

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,19 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./src/**/*.{html,ts}" // Asegúrate de incluir los archivos de Angular
-    ],
-    theme: {
-      fontFamily: {
-        'cinzel': ['Cinzel', 'sans-serif']
-      } ,
-      extend: {
-        colors: {
-          primary: '#1E40AF', // Azul fuerte
-          secondary: '#F59E0B', // Naranja vibrante
-        },
+  content: [
+    "./src/**/*.{html,ts}" // Asegúrate de incluir los archivos de Angular
+  ],
+  theme: {
+    fontFamily: {
+      'cinzel': ['Cinzel', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        primary: '#1E40AF', // Azul fuerte
+        secondary: '#F59E0B', // Naranja vibrante
       },
     },
-    plugins: [],
-  };
-  
+  },
+  plugins: [],
+};
