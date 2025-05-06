b072c282a32b6791cac29e80055ed009cceaba86
Camblor Cabero <denis.camblor-cabero.external@capgemini.com>
2025-05-06 10:26:29 +0200

    Add the aesthetic part he aesthetic part

---
+++ frontend/src/app/admin-dashboard/components/admin-products/admin-products.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'admin-products',
+  imports: [],
+  templateUrl: './admin-products.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AdminProductsComponent { }
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,5 +5,9 @@ export const routes: Routes = [
         path: '',
         loadChildren: () => import('./landing-page/landing-page.routes'),
     },
+    {
+        path: 'admin-dashboard',
+        loadChildren: () => import('./admin-dashboard/admin-dashboard.routes'), 
+    }
 
 ];
