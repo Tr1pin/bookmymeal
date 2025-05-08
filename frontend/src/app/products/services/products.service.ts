950fcb3e054b0e1795a067753fcfa0d5b1107040
Camblor Cabero <camblorcaberodenis@gmail.com>
2025-05-08 16:21:52 +0200

    Add products service

--- frontend/src/app/products/services/products.service.ts
+++ frontend/src/app/products/services/products.service.ts
@@ -1,20 +1,20 @@
-import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Product } from '../interfaces/Product';
-import { Observable, tap } from 'rxjs';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
-  private baseUrl: String = 'http://localhost:3001/productos/';
-  private http = inject( HttpClient );
-
-  constructor() { }
-
-  getProducts(): Observable<Product> {
-
-    return this.http.get<Product>(this.baseUrl+'')
-    .pipe(tap((resp) => console.log(resp)));
-  } 
+  private readonly baseUrl: string = 'http://localhost:3001/productos/';
+  private http = inject(HttpClient);
+  
+  
+  
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>('http://localhost:3001/productos/')
+      .pipe(tap((resp) => console.log(resp)));
+  }
 }
