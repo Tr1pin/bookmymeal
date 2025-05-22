5c5f492dd548844ac3360ac260d4e1f9a6caeb31
Camblor Cabero <denis.camblor-cabero.external@capgemini.com>
2025-05-22 12:49:08 +0200

    Update descripcion of what the endpoints do

--- backend/src/routes/data/productDataRoutes.js
+++ backend/src/routes/data/productDataRoutes.js
@@ -3,14 +3,14 @@ import { ProductController } from "../../controllers/data/productController.js";
 
 const router = Router();
 
-// Getters de Productos -> Operaciones R
+// Getters de Productos
 router.get('', ProductController.getAll);
 router.get('/productsImages', ProductController.getProductsImages);
 router.get('/id/:id', ProductController.getById);
 router.get('/nombre/:nombre', ProductController.getByNombre);
 router.get('/disponible/', ProductController.getByDisponibilidad);
 
-// Operaciones CUD
+// Operaciones Create, Update y Delete.
 router.post("", ProductController.crearProducto);
 router.put("", ProductController.actualizarProducto);
 router.delete("/:id", ProductController.eliminarProducto);
