44a3e92a8dd5b4604e86f0ce55c2063110d92de0
denis <camblorcaberodenis@gmail.com>
2025-05-23 17:47:30 +0200

    Update product service to delete, update,... products

--- frontend/src/app/products/services/products.service.ts
+++ frontend/src/app/products/services/products.service.ts
@@ -1,20 +1,49 @@
-import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { environment } from '../../../environments/environment';
 import { Product } from '../interfaces/Product';
-import { Observable } from 'rxjs';
+import { Observable, firstValueFrom } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
-  private readonly baseUrl: string = 'http://localhost:3001/productos/';
   private http = inject(HttpClient);
-  
-  
-  
+  private baseUrl = environment.baseUrl;
+
   getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>('http://localhost:3001/productos/')
+    return this.http.get<Product[]>(`${this.baseUrl}/productos/productsImages`)
       .pipe(tap((resp) => console.log(resp)));
+  }
+
+  getProduct(id: string): Observable<Product> {
+    return this.http.get<Product>(`${this.baseUrl}/productos/id/${id}`);
+  }
+
+  async createProduct(formData: FormData): Promise<Product> {
+    const response = await firstValueFrom(
+      this.http.post<Product>(`${this.baseUrl}/productos`, formData)
+    );
+    return response;
+  }
+
+  async updateProduct(formData: FormData): Promise<Product> {
+    const response = await firstValueFrom(
+      this.http.put<Product>(`${this.baseUrl}/productos`, formData)
+    );
+    return response;
+  }
+
+  async deleteProduct(id: string): Promise<void> {
+    await firstValueFrom(
+      this.http.delete<void>(`${this.baseUrl}/productos/${id}`)
+    );
+  }
+
+  async deleteProductImage(productId: string, filename: string): Promise<void> {
+    await firstValueFrom(
+      this.http.delete<void>(`${this.baseUrl}/productos/${productId}/images/${filename}`)
+    );
   }
 }
