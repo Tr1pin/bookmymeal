19e586e6b5997ee208be6d5cd12d3ecfe0e7a5de
denis <camblorcaberodenis@gmail.com>
2025-05-28 22:05:27 +0200

    Update reservation service to call delete, create, gets...

--- frontend/src/app/reservations/services/reservation.service.ts
+++ frontend/src/app/reservations/services/reservation.service.ts
@@ -1,8 +1,31 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, firstValueFrom } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { Reservation } from '../interfaces/Reservation';
+
+interface CreateReservationData {
+  nombre: string;
+  telefono: string;
+  fecha: string;
+  hora: string;
+  personas: number;
+}
+
+interface CreateReservationWithUserData {
+  usuario_id: string;
+  fecha: string;
+  hora: string;
+  personas: number;
+}
+
+interface UpdateReservationData {
+  id: string;
+  estado?: string;
+  fecha?: string;
+  hora?: string;
+  personas?: number;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +34,39 @@ export class RservationService {
     private readonly baseUrl: string = 'http://localhost:3001';
     private http = inject(HttpClient);
       
-      
-      
-      getReservas(): Observable<Reservation[]> {
-        return this.http.get<Reservation[]>(this.baseUrl+'/reservas/')
-          .pipe(tap((resp) => console.log(resp)));
+    getReservas(): Observable<Reservation[]> {
+      return this.http.get<Reservation[]>(this.baseUrl+'/reservas/');
+    }
+
+    getReservationById(id: string): Observable<Reservation> {
+      return this.http.get<Reservation>(`${this.baseUrl}/reservas/id/${id}`);
+    }
+
+    async createReservation(reservationData: CreateReservationData): Promise<any> {
+      const response = await firstValueFrom(
+        this.http.post<any>(`${this.baseUrl}/reservas`, reservationData)
+      );
+      return response;
+    }
+
+    async createReservationWithUser(reservationData: CreateReservationWithUserData): Promise<any> {
+      const response = await firstValueFrom(
+        this.http.post<any>(`${this.baseUrl}/reservas/with-user`, reservationData)
+      );
+      return response;
+    }
+
+    async updateReservation(reservationData: UpdateReservationData): Promise<any> {
+      const response = await firstValueFrom(
+        this.http.put<any>(`${this.baseUrl}/reservas`, reservationData)
+      );
+      return response;
+    }
+
+    async deleteReservation(id: string): Promise<any> {
+      const response = await firstValueFrom(
+        this.http.delete<any>(`${this.baseUrl}/reservas/${id}`)
+      );
+      return response;
     }
 } 
