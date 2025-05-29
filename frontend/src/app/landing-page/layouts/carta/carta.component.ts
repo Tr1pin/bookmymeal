1fc3a9c4718250b2ed963c24151f273a9155ced0
Camblor Cabero <camblorcaberodenis@gmail.com>
2025-05-29 11:32:51 +0200

    Add menu with product details and images

--- frontend/src/app/landing-page/layouts/carta/carta.component.ts
+++ frontend/src/app/landing-page/layouts/carta/carta.component.ts
@@ -1,9 +1,33 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ProductsService } from '../../../products/services/products.service';
+import { rxResource } from '@angular/core/rxjs-interop';
+
+import { CartaProductDetailsComponent } from './carta-product-details/carta-product-details.component';
 
 @Component({
   selector: 'app-carta',
-  imports: [],
+  imports: [CartaProductDetailsComponent],
   templateUrl: './carta.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CartaComponent { }
+export class CartaComponent { 
+  private productService = inject(ProductsService);
+
+  productsResource = rxResource({
+    request: () => ({}),
+    loader: ({ request }) =>{
+      return this.productService.getProducts();
+    }
+  });
+
+  fallbackImage = '../../../../../assets/images/static/empty.jpg';
+  
+  onImageError(event: Event) {
+    const img = event.target as HTMLImageElement;
+    if (img) img.src = this.fallbackImage;
+  }
+
+  getImageUrl(imageName: string): string {
+    return `http://localhost:3001/images/products/${imageName}`;
+  }
+}
