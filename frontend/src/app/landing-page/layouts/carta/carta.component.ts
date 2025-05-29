e18baf1370f0a9d6b72a51d24341a9c21e7e2bd7
denis <camblorcaberodenis@gmail.com>
2025-05-29 16:52:20 +0200

    Update carta to consume the product group by category

--- frontend/src/app/landing-page/layouts/carta/carta.component.ts
+++ frontend/src/app/landing-page/layouts/carta/carta.component.ts
@@ -1,12 +1,18 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
 import { ProductsService } from '../../../products/services/products.service';
 import { rxResource } from '@angular/core/rxjs-interop';
+import { Product } from '../../../products/interfaces/Product';
 
 import { CartaProductDetailsComponent } from './carta-product-details/carta-product-details.component';
+import { CommonModule } from '@angular/common';
+
+interface GroupedProducts {
+  [categoryName: string]: Product[];
+}
 
 @Component({
   selector: 'app-carta',
-  imports: [CartaProductDetailsComponent],
+  imports: [CartaProductDetailsComponent, CommonModule],
   templateUrl: './carta.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -15,10 +21,29 @@ export class CartaComponent {
 
   productsResource = rxResource({
     request: () => ({}),
-    loader: ({ request }) =>{
+    loader: ({ request }) => {
       return this.productService.getProducts();
     }
   });
+
+  groupedProducts = computed(() => {
+    const products = this.productsResource.value();
+    if (!products) {
+      return {} as GroupedProducts;
+    }
+    return products.reduce((acc, product) => {
+      const category = product.categoria_nombre || 'Otros';
+      if (!acc[category]) {
+        acc[category] = [];
+      }
+      acc[category].push(product);
+      return acc;
+    }, {} as GroupedProducts);
+  });
+
+  getCategoryNames(grouped: GroupedProducts | undefined | null): string[] {
+    return grouped ? Object.keys(grouped) : [];
+  }
 
   fallbackImage = '../../../../../assets/images/static/empty.jpg';
   
