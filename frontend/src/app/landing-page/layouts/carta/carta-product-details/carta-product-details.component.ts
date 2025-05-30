d0a376a7cd9162e9643395d2ed8ebb959aa063da
denis <camblorcaberodenis@gmail.com>
2025-05-30 10:38:09 +0200

    Update css in product details

--- frontend/src/app/landing-page/layouts/carta/carta-product-details/carta-product-details.component.ts
+++ frontend/src/app/landing-page/layouts/carta/carta-product-details/carta-product-details.component.ts
@@ -1,4 +1,7 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, inject } from '@angular/core';
+import { Product } from '../../../../products/interfaces/Product';
+import { CartService } from '../../../../cart/services/cart.service';
+import { ToastService } from '../../../../shared/services/toast.service';
 
 @Component({
   selector: 'carta-product-details',
@@ -6,10 +9,22 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CartaProductDetailsComponent {
+  private cartService = inject(CartService);
+  private toastService = inject(ToastService);
+
   fallbackImage = '../../../../../../assets/images/static/empty.jpg';
 
-  @Input() product: any;
+  @Input() product!: Product;
   @Input() getImageUrl!: (imageName: string) => string;
   @Input() onImageError!: (event: Event) => void;
+
+  addToCart(): void {
+    if (this.product) {
+      this.cartService.addItem(this.product, 1);
+      this.toastService.showToast(`'${this.product.nombre}' añadido al carrito`, 'success');
+    } else {
+      this.toastService.showToast('Error al añadir el producto', 'error');
+    }
+  }
 }
 
