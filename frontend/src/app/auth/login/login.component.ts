d388f982536054667afdc1f0d8c88df5fc472aec
denis <camblorcaberodenis@gmail.com>
2025-06-03 21:18:48 +0200

    Delete and move some files

--- frontend/src/app/auth/login/login.component.ts
+++
@@ -1,8 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'app-login',
-  imports: [],
-  templateUrl: './login.component.html',
-})
-export class LoginComponent { }
