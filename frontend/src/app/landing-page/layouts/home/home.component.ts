e22e3dd44c2e7226593e68fdfcedb0c5540d2152
denis <camblorcaberodenis@gmail.com>
2025-06-03 10:08:38 +0200

    Update home.ts to use carta-product-details styles

--- frontend/src/app/landing-page/layouts/home/home.component.ts
+++ frontend/src/app/landing-page/layouts/home/home.component.ts
@@ -10,7 +10,7 @@ import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [ CommonModule, RouterLink],
+  imports: [ CommonModule, RouterLink, CartaProductDetailsComponent],
   templateUrl: './home.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
