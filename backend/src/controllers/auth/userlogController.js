5eee2f65c71c428c21e078b7d8c7dd6cd7de0b31
Camblor Cabero <denis.camblor-cabero.external@capgemini.com>
2025-06-05 12:16:41 +0200

    Add to log and register email comfirmation

--- backend/src/controllers/auth/userlogController.js
+++ backend/src/controllers/auth/userlogController.js
@@ -1,4 +1,5 @@
 import { UserAuthModel } from "../../models/auth/userlogModel.js";
+import { EmailService } from "../../services/email.service.js";
 
 export class UserAuthController {
     static async login(req, res) {
@@ -6,6 +7,7 @@ export class UserAuthController {
           const { email, password } = req.body;
           const resp = await UserAuthModel.login({ email, password });
 
+          await EmailService.sendEmail({ to: email, toName: resp.nombre, subject: "login"});
           res.status(200).json(resp);
       } catch (err) {
           res.status(500).json({ error: err.message });
@@ -17,6 +19,7 @@ export class UserAuthController {
             const { nombre, email, password, telefono } = req.body;
             const newUser = await UserAuthModel.register({ nombre, email, password, telefono });
 
+            await EmailService.sendEmail({ to: email, toName: nombre, subject: "registro"});
             res.status(201).json(newUser);
         } catch (error) {
           res.status(500).json({ error: error.message });
